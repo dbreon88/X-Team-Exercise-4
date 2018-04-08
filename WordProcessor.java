1e179b6560f853ddf673854c8aae0aadbafc55d1
Steven Berry <berrste@gmail.com>
2018-04-08 15:59:07 -0500

    Got rid of the random 'd'

--- WordProcessor.java
+++ WordProcessor.java
@@ -68,7 +68,7 @@ public class WordProcessor {
 		return null;
 	}
 	
-	d/**
+	/**
 	 * Adjacency between word1 and word2 is defined by:
 	 * if the difference between word1 and word2 is of
 	 * 	1 char replacement
