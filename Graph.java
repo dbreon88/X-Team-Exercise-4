9e3b407e461487ac93b840ab6a430fb8f1ce8396
dbreon88 <37003326+dbreon88@users.noreply.github.com>
2018-04-08 15:58:16 -0500

    Added Vertex subclass

--- Graph.java
+++ Graph.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Set;
@@ -11,6 +12,21 @@ import java.util.Set;
  * 
  */
 public class Graph<E> implements GraphADT<E> {
+    
+    class Vertex<T> {
+        T data;
+        ArrayList<Vertex<T>> neighbors; 
+        ArrayList<ArrayList<T>> shortestPaths;
+        ArrayList<Integer> shortestDistances; 
+        
+        public Vertex(T data){
+            this.data = data; 
+            this.neighbors = new ArrayList<Vertex<T>>();
+            this.shortestPaths = new ArrayList<ArrayList<T>>();
+            this.shortestDistances = new ArrayList<Integer>();
+  
+        }
+    }
     
     /**
      * Instance variables and constructors
