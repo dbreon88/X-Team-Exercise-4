6fd1eaf6e53cc26f926582acd9f7bd0cfed67cd3
Steven Berry <berrste@gmail.com>
2018-04-12 19:52:33 -0500

    Added a bunch of tests for the WordProcessor.isAdjacent method

--- GraphProcessorTest.java
+++ GraphProcessorTest.java
@@ -28,7 +28,7 @@ public class GraphProcessorTest {
     @Before
     public void setUp() throws Exception {
         this.processor = new GraphProcessor();
-        processor.populateGraph("word_list.txt");
+        processor.populateGraph("/Users/stevenberry/Downloads/word_list.txt");
     }
     
     @After
@@ -36,5 +36,63 @@ public class GraphProcessorTest {
         this.processor = null;
     }
     
+    @Test
+    public void testIsAdjacentIfWordsAreOneLetterOffByAdding() throws Exception {
+        String[] strings = {"test", "tests"};
+        assertEquals("words are one character off by adding", true, WordProcessor.isAdjacent(strings[0], strings[1]));
+    }
+    
+    @Test
+    public void testIsAdjacentIfWordsAreOneLetterOffBySubtracting() throws Exception {
+        String[] strings = {"tests", "test"};
+        assertEquals("words are one character off by subtracting", true, WordProcessor.isAdjacent(strings[0], strings[1]));
+    }
 
+    @Test
+    public void testIsAdjacentIfWordsAreOneLetterOffByChanging() throws Exception {
+        String[] strings = {"test", "tost"};
+        assertEquals("words are one character off by changing", true, WordProcessor.isAdjacent(strings[0], strings[1]));
+    }
+    
+    @Test
+    public void testIsAdjacentIfWordsAreMoreThanOneCharacterOffByAdding() throws Exception {
+        String[] strings = {"test", "testin"};
+        assertEquals("words are more than one character off by adding", false, WordProcessor.isAdjacent(strings[0], strings[1]));
+    }
+    
+    @Test
+    public void testIsAdjacentIfWordsAreMoreThanOneCharacterOffBySubtracting() throws Exception {
+        String[] strings = {"testin", "test"};
+        assertEquals("words are more than one character off by adding", false, WordProcessor.isAdjacent(strings[0], strings[1]));
+    }
+    
+    @Test
+    public void testIsAdjacentIfWordsAreMoreThanOneCharacterOffByChanging() throws Exception {
+        String[] strings = {"tell", "test"};
+        assertEquals("words are more than one character off by adding", false, WordProcessor.isAdjacent(strings[0], strings[1]));
+    }
+    
+    @Test
+    public void testIsAdjacentIfWordsAreMoreThanOneCharacterOffByAddingAndChanging() throws Exception {
+        String[] strings = {"test", "tasty"};
+        assertEquals("words are more than one character off by adding and changing", false, WordProcessor.isAdjacent(strings[0], strings[1]));
+    }
+    
+    @Test
+    public void testIsAdjacentIfWordsAreMoreThanOneCharacterOffBySubtractingAndChanging() throws Exception {
+        String[] strings = {"test", "tat"};
+        assertEquals("words are more than one character off by subtracting and changing", false, WordProcessor.isAdjacent(strings[0], strings[1]));
+    }
+    
+    @Test
+    public void testIsAdjacentIfWordsAreMoreThanOneCharacterOffByChangingTwice() throws Exception {
+        String[] strings = {"test", "toot"};
+        assertEquals("words are more than one character off by changing twice", false, WordProcessor.isAdjacent(strings[0], strings[1]));
+    }
+    
+    @Test
+    public void testIsAdjacentIfWordsAreDuplicates() throws Exception {
+        String[] strings = {"test", "test"};
+        assertEquals("words are more than one character off by adding", false, WordProcessor.isAdjacent(strings[0], strings[1]));
+    }
 }
