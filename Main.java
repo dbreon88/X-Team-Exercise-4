20fa5fb599d54f942f343ba32b6b642055dbb9fb
dbreon88 <37003326+dbreon88@users.noreply.github.com>
2018-04-12 19:04:40 -0500

    Added populate graph trial

--- Main.java
+++ Main.java
@@ -1,9 +1,16 @@
+import java.io.IOException;
 
 public class Main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
+    public static void main(String[] args) {
+       GraphProcessor proc = new GraphProcessor();
+       try {
+       proc.populateGraph("/Users/administrator/eclipse-workspace/p4/src/word_list.txt");
+       }
+       catch (IOException e) {
+           System.out.println(e.getMessage());
+       }
+       System.out.println(proc.toString());
+    }
 
 }
