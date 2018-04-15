f8b270535fd262b7d8284f8dd8ed09ecd2637342
Elliot <braemelliot@gmail.com>
2018-04-15 13:31:22 -0500

    get shortest distance works

--- GraphProcessor.java
+++ GraphProcessor.java
@@ -101,6 +101,7 @@ public class GraphProcessor {
                 }
             }
         }
+        /**
         // iterator
         Iterator<String> itr = this.graph.getAllVertices().iterator();
         // for each in iterato
@@ -116,6 +117,7 @@ public class GraphProcessor {
         	System.out.println();
 
         }
+        */
         
         return counter;
     
@@ -228,11 +230,24 @@ public class GraphProcessor {
             		continue;
             	}
                 for (int j = 0; j < words.size(); j++){
-                	System.out.println("i, j: " + dist[i][j]);
-                	System.out.println("i, k: " + dist[i][k]);
-                	System.out.println("k, j: " + dist[k][j]);
+                	
+                	System.out.println();
+                	System.out.println("current i: " + words.get(i));
+                	System.out.println("goal j: " + words.get(j));
+                	System.out.println("next k, j : " + next[k][j]);
+                	
+                	if (words.get(i).equals(words.get(j))){
+                		dist[i][j] = 0;
+                		next[i][j] = null;
+                	}
                 	// if a shorter distance between i and j is found 
-                    if (dist[i][j] > dist[i][k] + dist[k][j]){
+                    if (dist[i][j] > dist[i][k] + dist[k][j]) {
+                    	
+                    	System.out.println("word k: " + words.get(k));
+                    	System.out.println("dist i, j: " + dist[i][j]);
+                    	System.out.println("dist i, k: " + dist[i][k]);
+                    	System.out.println("dist k, j : " + dist[k][j]);
+                    	
                         // update the distance
                         dist[i][j] = dist[i][k] + dist[k][j];
                         // fix the link
